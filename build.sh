157d8c61c707d235c9a2aae2f0778ba84345e21b
Sam Beckett <becketts@byu.edu>
2019-05-02 13:35:12 -0600

    add 'go get' to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 go get github.com/gin-gonic/gin
+go get github.com/jmoiron/sqlx
+go get github.com/lib/pq
+go get gopkg.in/guregu/null.v3/zero
+
 cd /go/src/kapacitor-alerts-api/
-go build server.go
+go build
